1776926143c058375c8fa85b80671c050ed3eb77
RahulRaoMR <royalrahulraomr9103@gmail.com>
2026-02-20 17:03:39 +0530

    fix: db + hall model

--- app/api/vendor/register/route.js
+++ app/api/vendor/register/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import {connectDB } from "@/lib/mongodb";
+import connectDB from "@/lib/mongodb";
 import Vendor from "@/models/Vendor";
 
 export async function POST(req) {
